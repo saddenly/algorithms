55f6ccd49e579751184d6b05982fd9deeb968f58
saddenly <rustemandasov6@gmail.com>
2021-09-17 19:52:14 +0600

    initial

---
+++ src/bubbleSort/Main.java
@@ -0,0 +1,28 @@
+package bubbleSort;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {-2, 5, 3, 10, 0, 11, -5};
+        System.out.println(Arrays.toString(bubbleSort(nums)));
+    }
+
+    public static int[] bubbleSort(int[] nums) {
+        boolean sorted = false;
+
+        while (!sorted) {
+            sorted = true;
+
+            for (int i = 0; i < nums.length - 1; i++) {
+                if (nums[i] > nums[i + 1]) {
+                    int temp = nums[i];
+                    nums[i] = nums[i + 1];
+                    nums[i + 1] = temp;
+                    sorted = false;
+                }
+            }
+        }
+        return nums;
+    }
+}
---
+++ src/insertionSort/Main.java
@@ -0,0 +1,23 @@
+package insertionSort;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {-2, 5, 3, 10, 0, 11, -5};
+        System.out.println(Arrays.toString(insertionSort(nums)));
+    }
+
+    public static int[] insertionSort(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            int current = nums[i];
+            int j = i - 1;
+            while (j >= 0 && current < nums[j]) {
+                nums[j + 1] = nums[j];
+                j--;
+            }
+            nums[j + 1] = current;
+        }
+        return nums;
+    }
+}
