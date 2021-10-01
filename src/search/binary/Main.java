d2fb7f747aff7827a496a7d2ee1962d6a3e25d4c
saddenly <rustemandasov6@gmail.com>
2021-10-01 23:00:50 +0600

    search algorithms: linear and binary algorithms

---
+++ src/search/binary/Main.java
@@ -0,0 +1,37 @@
+package search.binary;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 5, 10, 11, 15, 26};
+        int result = binarySearch(nums, 0, nums.length - 1, 126);
+        if (result == -1) {
+            System.out.println("Element is not found");
+        } else {
+            System.out.println("Element found at index " + result);
+        }
+    }
+
+    public static int binarySearch(int[] arr, int startIndex, int endIndex, int x) {
+        if (endIndex >= startIndex) {
+            int mid = startIndex + (endIndex - startIndex) / 2;
+
+            // If the element is present at the
+            // middle itself
+            if (arr[mid] == x)
+                return mid;
+
+            // If element is smaller than mid, then
+            // it can only be present in left subarray
+            if (arr[mid] > x)
+                return binarySearch(arr, startIndex, mid - 1, x);
+
+            // Else the element can only be present
+            // in right subarray
+            return binarySearch(arr, mid + 1, endIndex, x);
+        }
+
+        // We reach here when element is not present
+        // in array
+        return -1;
+    }
+}
---
+++ src/search/linear/Main.java
@@ -0,0 +1,18 @@
+package search.linear;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {1, 5, 3, 10, 0, 11, 26};
+        System.out.println(linearSearch(nums, 72));
+    }
+
+    public static int linearSearch(int[] nums, int x) {
+        for (int num : nums) {
+            if (num == x) {
+                return num;
+            }
+        }
+
+        return -1;
+    }
+}
